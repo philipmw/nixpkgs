ac605a87e17856d39ff46cb56ed057c2c5bcd5bc
Bobby Rong <rjl931189261@126.com>
2022-07-18 11:24:45 +0800

    cinnamon.cinnamon-desktop: 5.2.0 -> 5.4.1

--- pkgs/desktops/cinnamon/cinnamon-desktop/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-desktop/default.nix
@@ -18,13 +18,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-desktop";
-  version = "5.2.0";
+  version = "5.4.1";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-gOlSmcHjBjnLdDpgC5mZ4M3eUBTG3BuET6Kr/Xby14A=";
+    hash = "sha256-F4Jc5NtsastD2VKKHfUZzWe1OwzcL0z538NifmFtrQ4=";
   };
 
   outputs = [ "out" "dev" ];
