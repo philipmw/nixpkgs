246c1ece9534a1205bc702dbd558615ec3ea2e7f
Bobby Rong <rjl931189261@126.com>
2022-07-18 14:12:21 +0800

    cinnamon.muffin: 5.2.0 -> 5.4.2
    Rebased on mutter 3.36.0-0ubuntu0.20.04.1.

--- pkgs/desktops/cinnamon/muffin/default.nix
+++ pkgs/desktops/cinnamon/muffin/default.nix
@@ -1,95 +1,70 @@
-{ fetchFromGitHub
-, cinnamon-desktop
-, glib
-, file
-, gnome
-, gnome-doc-utils
-, fetchpatch
-, gobject-introspection
-, gtk3
-, intltool
-, json-glib
-, libinput
-, libstartup_notification
-, libXtst
-, libxkbcommon
-, pkg-config
+{ stdenv
 , lib
-, stdenv
+, fetchFromGitHub
+, cinnamon-desktop
+, dbus
+, desktop-file-utils
+, glib
+, gobject-introspection
+, graphene
+, gtk3
+, json-glib
+, libcanberra-gtk3
+, libgnomekbd
+, libgudev
+, libstartup_notification
+, libXdamage
+, libxkbcommon
+, libXtst
+, mesa
+, meson
+, ninja
+, pipewire
+, pkg-config
+, python3
 , udev
-, xorg
 , wrapGAppsHook
-, pango
-, cairo
-, gtk-doc
-, docbook_xsl
-, docbook_xml_dtd_43
-, docbook_xml_dtd_42
-, docbook_xml_dtd_412
-, autoconf
-, automake
-, gettext
-, libtool
 }:
-
-# it's a frankensteins monster with some cinnamon sparkles added on top of it
 
 stdenv.mkDerivation rec {
   pname = "muffin";
-  version = "5.2.0";
+  version = "5.4.2";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-WAp0HbfRtwsPjJX1kPBqUStqLaudQPZ8E+h4jmggmw8=";
+    hash = "sha256-MT26Kk5xpdAK2dSaLAN0Ih7qeWVDEINllc3UbiIVZjY=";
   };
 
-  buildInputs = [
-    gtk3
-    glib
-    pango
-    cairo
-    json-glib
-    cinnamon-desktop
-    xorg.libXcomposite
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libxkbfile
-    xorg.xkeyboardconfig
-
-    libxkbcommon
-    gnome.zenity
-    libinput
-    libstartup_notification
-    libXtst
-    udev
-    gobject-introspection
-  ];
-
   nativeBuildInputs = [
-    autoconf
-    automake
-    gettext
-    libtool
-    wrapGAppsHook
+    desktop-file-utils
+    gobject-introspection
+    meson
+    ninja
     pkg-config
-    intltool
-
-    gnome-doc-utils
-    gtk-doc
-    docbook_xsl
-    docbook_xml_dtd_43
-    docbook_xml_dtd_42
-    docbook_xml_dtd_412
+    python3
+    wrapGAppsHook
   ];
 
-  preConfigure = ''
-    NOCONFIGURE=1 ./autogen.sh
-  '';
+  buildInputs = [
+    cinnamon-desktop
+    dbus
+    glib
+    graphene
+    gtk3
+    json-glib
+    libcanberra-gtk3
+    libgnomekbd
+    libgudev
+    libstartup_notification
+    libXdamage
+    libxkbcommon
+    libXtst
+    mesa
+    pipewire
+    udev
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/linuxmint/muffin";
